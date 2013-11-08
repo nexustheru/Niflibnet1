01500edd21b51a14b85248b468ae7de9ca2e15e0
nexustheru <sehindel_19@hotmail.com>
2013-11-08 14:40:53 +0100

    ..

--- NiGeomtryData.vb
+++ NiGeomtryData.vb
@@ -22,4 +22,103 @@
     Public uvsets() As TexCoord
     Public consistencyflags As Enums.ConsistencyType
     Public additionaldata() As NiAdditionalData
+
+    Public Overloads Function asstring(verboose As Boolean) As String
+        Return Me.ToString()
+    End Function
+    Public Overloads Function returntype() As Type
+        Return Me.GetType()
+    End Function
+    Public Function GetVertexCount() As Integer
+
+    End Function
+    Public Function GetUVSetCount() As Short
+
+    End Function
+    Public Sub SetUVSetCount(n As Integer)
+
+    End Sub
+    Public Function GetVertexIndexCount() As Integer
+
+    End Function
+    Public Function GetCenter() As Vector3
+
+    End Function
+    Public Function GetRadius() As Single
+
+    End Function
+    Public Sub SetBound(center As Vector3, radius As Single)
+
+    End Sub
+    Public Function GetVertices() As Vector3()
+
+    End Function
+    Public Function GetNormals() As Vector3()
+
+    End Function
+    Public Function GetColors() As Color4
+
+    End Function
+    Public Function GetUVSet(index As Integer) As TexCoord
+
+    End Function
+    Public Function GetVertexIndices() As Integer
+
+    End Function
+    Public Function GetUVSetIndex(maxMapChannel As Integer) As Integer
+
+    End Function
+    Public Sub SetVertices(inv As Vector3())
+
+    End Sub
+
+    Public Sub SetNormals(inn As Vector3())
+
+    End Sub
+    Public Sub SetVertexColors(inc As Color4())
+
+    End Sub
+    Public Sub SetUVSet(index As Integer, int As TexCoord())
+
+    End Sub
+    Public Sub SetVertexIndices(inv As Integer)
+
+    End Sub
+
+    Public Sub SetUVSetMap(iv As Integer, ivs As Integer)
+
+    End Sub
+    Public Sub Transform(transform As matrix44)
+
+    End Sub
+    Public Function GetConsistencyFlags() As Enums.ConsistencyType
+
+    End Function
+    Public Sub SetConsistencyFlags(value As Enums.ConsistencyType)
+
+    End Sub
+    Public Function GetTspaceFlag() As Byte
+
+    End Function
+    Public Sub SetTspaceFlag(value As Byte)
+
+    End Sub
+    Public Function GetHasNormals() As Boolean
+
+    End Function
+    Public Sub SetHasNormals(value As Boolean)
+
+    End Sub
+    Public Function GetBinormals() As Vector3()
+
+    End Function
+    Public Sub SetBinormals(value As Vector3)
+
+    End Sub
+    Public Function GetTangents() As Vector3()
+
+    End Function
+    Public Sub SetTangents(value As Vector3)
+
+    End Sub
 End Class
